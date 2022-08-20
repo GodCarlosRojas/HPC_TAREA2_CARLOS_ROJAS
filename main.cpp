75c637b170d0d5e99a2ae5a595278fa445716467
GodCarlosRojas <109989522+GodCarlosRojas@users.noreply.github.com>
2022-08-20 18:50:47 -0500

    PUNTO 4

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 /**********************************************************
  * FEcha: 23 Agosto de 2022
  * Autor: Carlos Andres ROJas Rocha
- * TEma: PUNTO 2
+ * TEma: PUNTO 4
  * Materia: INtroduccion a la HPC
  * *******************************************************/
 #include <iostream>
 #include <vector>
 #include <eigen3/Eigen/Dense>
+#include <math.h>
 //Como la matricez que se vana trabajar son 2x2 se inicializan asi
 
 typedef Eigen::Matrix<float,2,2> MiMatriz2x2f;
@@ -18,18 +19,20 @@ int main()
     MiMatriz2x2f mB;
     MiMatriz2x2f mX;
 
-    std::cout << "\n------------PUNTO DOS-------------------" << std::endl;
+    std::cout << "\n------------PUNTO CUATRO-------------------" << std::endl;
     //Se requiere llenar la matriz con los numeros que se indican que valen a la
     // matriz A y a la matriz B
      std::cout << "\n Matriz A"<< std::endl;
-    mA << 0, -1, 2, 1;
+    mA << 2, 1, -4, -3;
     std::cout<< mA <<std::endl;
     std::cout << "\n Matriz B"<< std::endl;
-    mB << 1, 2, 3, 4;
+    mB << 2, 2, 6, 4;
     std::cout<<mB<<std::endl;
-    //Ahora ya teniendo la matriz A y B se operan X=(3BA-4A)/2
+    //Ahora ya teniendo la matriz A y B se operan X=B/A
     std::cout << "\n Matriz X"<< std::endl;
-    mX=((3*mB*mA)-(4*mA))/2;
+    //Se requiere calcular la inversa de A para si pmultiplicarlo por B
+    //Esto toca ya que la division de matricez no es posible
+    mX=mB*mA.inverse();
     std::cout<<mX<<std::endl;
     //Se imprime el resultado de la operacion de matricez
     return 0;
