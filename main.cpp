4c0176d451f8ade8fb63f955a1daf5824d90c276
GodCarlosRojas <109989522+GodCarlosRojas@users.noreply.github.com>
2022-08-20 18:52:23 -0500

    PUNTO 6

--- main.cpp
+++ main.cpp
@@ -1,35 +1,36 @@
 /**********************************************************
  * FEcha: 23 Agosto de 2022
  * Autor: Carlos Andres ROJas Rocha
- * TEma: PUNTO 2
+ * TEma: PUNTO 6
  * Materia: INtroduccion a la HPC
  * *******************************************************/
 #include <iostream>
 #include <vector>
 #include <eigen3/Eigen/Dense>
-//Como la matricez que se vana trabajar son 2x2 se inicializan asi
+#include <math.h>
+//Como la matricez que se vana trabajar son 3x3 se inicializan asi
 
-typedef Eigen::Matrix<float,2,2> MiMatriz2x2f;
+typedef Eigen::Matrix<float,3,3> MiMatriz3x3f;
 
 int main()
 {
 
-    MiMatriz2x2f mA;
-    MiMatriz2x2f mB;
-    MiMatriz2x2f mX;
+    MiMatriz3x3f mA;
+    MiMatriz3x3f mB;
+    MiMatriz3x3f mX;
 
-    std::cout << "\n------------PUNTO DOS-------------------" << std::endl;
+    std::cout << "\n------------PUNTO SEIS-------------------" << std::endl;
     //Se requiere llenar la matriz con los numeros que se indican que valen a la
     // matriz A y a la matriz B
      std::cout << "\n Matriz A"<< std::endl;
-    mA << 0, -1, 2, 1;
+    mA << 1, 5, -1, -1, 2, 2, 0, -3, 3;
     std::cout<< mA <<std::endl;
     std::cout << "\n Matriz B"<< std::endl;
-    mB << 1, 2, 3, 4;
+    mB << -1, -4, 3, 1, -2, -2, -3, 3, -5;
     std::cout<<mB<<std::endl;
-    //Ahora ya teniendo la matriz A y B se operan X=(3BA-4A)/2
+    //Ahora ya teniendo la matriz A y B se operan X=A+3B
     std::cout << "\n Matriz X"<< std::endl;
-    mX=((3*mB*mA)-(4*mA))/2;
+    mX=mA+(3*mB);
     std::cout<<mX<<std::endl;
     //Se imprime el resultado de la operacion de matricez
     return 0;
